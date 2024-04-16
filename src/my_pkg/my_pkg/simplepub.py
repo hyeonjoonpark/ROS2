6969225c47d76ed3de2690fdeed0ee126a497546
hyeonjoonpark <pjjoon1379@gmail.com>
2024-04-16 17:56:34 +0900

    ADD: class

--- src/my_pkg/my_pkg/simplepub.py
+++ src/my_pkg/my_pkg/simplepub.py
@@ -1,5 +1,16 @@
-def main():
-    print('Hello, World!')
+import rclpy as rp
+import rclpy.node import Node # type: ignore
 
+def print_hello():
+    print('Hello World!')
+
+
+def main():
+    rp.init()
+    node = Node('simplepub')
+
+    rp.spin(node)
+    node.create_timer(1.0, print_hello)
+    
 if __name__ == '__main__':
     main()
