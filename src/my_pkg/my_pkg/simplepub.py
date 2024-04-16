908b23b1339b6cd0c72577625548f397c958d41f
hyeonjoonpark <pjjoon1379@gmail.com>
2024-04-16 18:04:12 +0900

    add : twist

--- src/my_pkg/my_pkg/simplepub.py
+++ src/my_pkg/my_pkg/simplepub.py
@@ -1,14 +1,20 @@
 import rclpy as rp
 import rclpy.node import Node # type: ignore
+from geometry_msgs.msg import Twist
+
 
 
 class SimplePub(Node):
     def __init__(self):
         super().__init__('simplepub')
-        self.create_timer(1.0, self.print_hello)
+        self.create_timer(1, self.print_hello)
+        self.pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
     def print_hello(self):
-        print('Hello World!')
+        msg = Twist()
+        msg.linear.x = 0.5
+        msg.linear.z = 0.5
+        self.pub.publish(msg)
 
 
 def main():
