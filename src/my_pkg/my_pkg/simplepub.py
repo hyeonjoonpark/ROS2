d91861eb292b891b3c1fdc1471dfd513a7683d9a
hyeonjoonpark <pjjoon1379@gmail.com>
2024-04-16 17:57:29 +0900

    fix node

--- src/my_pkg/my_pkg/simplepub.py
+++ src/my_pkg/my_pkg/simplepub.py
@@ -1,14 +1,19 @@
 import rclpy as rp
 import rclpy.node import Node # type: ignore
 
-def print_hello():
-    print('Hello World!')
+
+class SimplePub(Node):
+    def __init__(self):
+        super().__init__('simplepub')
+        self.create_timer(1.0, self.print_hello)
+
+    def print_hello(self):
+        print('Hello World!')
 
 
 def main():
     rp.init()
-    node = Node('simplepub')
-
+    node = SimplePub()
     rp.spin(node)
     node.create_timer(1.0, print_hello)
     
