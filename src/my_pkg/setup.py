a23294a668edb614f95417613bf0d54b545c3f8a
hyeonjoonpark <pjjoon1379@gmail.com>
2024-04-17 15:55:23 +0900

    Setup

--- src/my_pkg/setup.py
+++ src/my_pkg/setup.py
@@ -1,28 +1,31 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_pkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='hyeonjoonpark',
-    maintainer_email='hyeonjoonpark@todo.todo',
+    maintainer='aa',
+    maintainer_email='freshmea@naver.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'simplepub = my_pkg.simplepub:main'
-            'simplepub2 = my_pkg.simplesub:main'
-            'simpletimepub = my_pkg.simpletimepub:main'
+            'simplepub = my_pkg.simplepub:main',
+            'simplesub = my_pkg.simplesub:main',
+            'simpletimepub = my_pkg.simpletimepub:main',
             'simpletimesub = my_pkg.simpletimesub:main'
         ],
     },
